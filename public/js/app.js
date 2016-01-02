d805261a7f2ebcf177ff35b1e6b991b9c9389f2a
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-01-02 21:22:00 +0530

    Added name to chat users

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var socket = io();
+var name = getParameterByName('name') || 'Anonymous';
+var room = getParameterByName('room');
 
 socket.on('connect', function(){
 	console.log('Connected to socket.io server');
@@ -7,7 +9,7 @@ socket.on('connect', function(){
 socket.on('message', function(message){
 	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message: ' + message.text);
-	$('.messages').append('<p>'+ message.text +' <small class="float-right">'+ momentTimestamp.local().format('h:mma') +'</small></p>');
+	$('.messages').append('<p><strong>'+ message.name + '</strong><br />' + message.text +' <small class="float-right">'+ momentTimestamp.local().format('h:mma') +'</small></p>');
 });
 
 var $form = $('#message-form');
@@ -18,8 +20,17 @@ $form.on('submit',function(evt){
 	var message = $form.find('[name=message]');
 
 	socket.emit('message', {
-		text: message.val()
+		text: message.val(),
+		name: name
 	});
 
 	message.val('');
 });
+
+// special function to parse queryParams
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? undefined : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.on('connection', function(socket){
 	});
 
 	socket.emit('message', {
+		name: 'System',
 		text: 'Welcome to the chat application',
 		timestamp: moment().valueOf()
 	});
