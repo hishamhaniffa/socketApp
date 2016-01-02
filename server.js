35e3509db738391b5ac20fcf2f7aaa0dbef46031
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-01-02 16:48:53 +0530

    Added socket.io on server and client

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var http = require('http').Server(app);
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+io.on('connection', function(){
+	console.log('New user connected');
+});
 
 http.listen(PORT,function(){
 	console.log('Server started at port: ' + PORT + '!');
